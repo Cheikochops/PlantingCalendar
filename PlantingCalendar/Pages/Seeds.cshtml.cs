80b1fe77045faecf9892b3d07bd8af47c9e33da3
Cheikochops <caitlinbaxter99@gmail.com>
2023-03-16 20:30:48 +0000

    Added pages
    Added some basic cshtml pages

---
+++ PlantingCalendar/Pages/Seeds.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlantingCalendar.Pages
+{
+    public class SeedsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
